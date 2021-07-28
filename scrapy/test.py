966eb2fa31f5c8b87b50323f750005366e5c38d8
EtherealSymbol <2391892926@qq.com>
2021-07-28 17:48:41 +0800

    Create test.py

---
+++ scrapy/test.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "http://app1.nmpa.gov.cn/data_nmpa/face3/base.jsp?" \
+      "tableId=23&tableName=TABLE23&title=GMP%C8%CF%D6%A4&bcId=152911797956855568058860206743"
+
+Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) '
+                         'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'}
+
+res = requests.get(url, headers=Headers)
+print("状态码： ", res.status_code)
+
+
+Content = res.text
+print("Content: \n", Content)
+
+
